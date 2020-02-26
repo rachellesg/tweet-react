1521952e41c34d76f17d535cc22d7967977bf018
rachellesg <rachelle.yeo@outlook.com>
2020-02-26 21:42:09 +0800

    tweet cards are done

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ app.get('/banana', (request, response)=>{
 app.get('/react', (req, res) => {
   const myHtml = `
     <html>
+    <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
       <body>
         <h1>Wow, react</h1>
         <div id="app"></div>
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import tweets from 'tweets'
+import tweets from 'tweets';
 
 class App extends React.Component {
   render() {
     const allTweets = tweets.tweets;
     console.log(allTweets);
     return (
-      <div>
+      <div className="container">
         <Tweets hello={allTweets}/>
       </div>
     );
@@ -18,18 +18,29 @@ class App extends React.Component {
 class Tweets extends React.Component {
   render () {
     let tweetList = this.props.hello.map(tweet => {
-      return <li>++ <Tweet name={tweet.text} content={tweet.content} date={tweet.created_at} /></li>
+      return <div className="col-12 mb-3 d-flex card"><Tweet profileimage={tweet.user.profile_image_url} username={tweet.user.screen_name} name={tweet.user.name} content={tweet.text} date={tweet.created_at} retweetCount={tweet.retweet_count} favCount={tweet.favorite_count}/></div>
     });
     return (
-      <div className="tweets">{tweetList}</div>
+      <div className="row tweets">{tweetList}</div>
     );
   }
 }
 
 class Tweet extends React.Component {
   render () {
+    // var timestamp = Moment(this.props.date);
+    // console.log(timestamp)
     return (
-      <div className="username">{this.props.name} {this.props.content} {this.props.date}</div>
+      <div className="content row">
+        <div className="profile-image col-3"><img src={this.props.profileimage} /></div>
+        <div class="col-9">
+          <div className="username">{this.props.name} / {this.props.username}</div> 
+          <div className="text">{this.props.content} </div>
+          <div className="date">{this.props.date}</div>
+          <br />
+          Fav: {this.props.favCount} | Retweets: {this.props.retweetCount}
+        </div>
+      </div>
     );
   }
 }
