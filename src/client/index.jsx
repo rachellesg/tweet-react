68e4b1121c45ec2a45f126b59283250e15eb2222
rachellesg <rachelle.yeo@outlook.com>
2020-02-26 17:48:40 +0800

    updated initial commit, using fruits as an example

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -5,10 +5,36 @@ import tweets from 'tweets'
 
 class App extends React.Component {
   render() {
+    const Tweets = tweets.tweets;
+    console.log(Tweets);
+    const listOfFruits = [
+      "apples",
+      "bananas",
+      "pineapple"
+    ];
     return (
       <div>
-        tweets
+        <Fruits fruits={listOfFruits}/>
       </div>
+    );
+  }
+}
+
+class Fruits extends React.Component {
+  render () {
+    let fruitListItems = this.props.fruits.map(fruit => {
+      return <li><Fruit name={fruit} /></li>
+    });
+    return (
+      <div className="tweets">{fruitListItems}</div>
+    );
+  }
+}
+
+class Fruit extends React.Component {
+  render () {
+    return (
+      <div className="username">{this.props.name}</div>
     );
   }
 }
